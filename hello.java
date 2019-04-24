29e0fede3488e1120cc0e3bf1ab3655d1c583ba3
Jcduhdt <15150651313@126.com>
2019-04-24 19:30:34 +0800

    Hello java!

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ class Hello
   public static void main(String[] args)
   {
     System.out.println("Hello World!");
+    System.out.print("Hello Java!");
     System.out.print("Keep running!");
   }
 }
