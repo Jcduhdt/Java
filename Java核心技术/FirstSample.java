a32f8e62f1973227cbbe73777038deff03fd0432
Jcduhdt <15150651313@126.com>
2019-05-06 00:09:59 +0800

    Create FirstSample.java

---
+++ Java核心技术/FirstSample.java
@@ -0,0 +1,12 @@
+/**
+ * time:2019-5-5
+ * author: Zhang Xiong
+ * This is the first sample program in Core Java Chapter 3
+ */
+public class FirstSample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("We will not use 'Hello World!'");
+    }
+}
