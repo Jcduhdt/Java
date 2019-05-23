d414a501e8f14a359c3baeb101ce137137f729b5
Jcduhdt <15150651313@126.com>
2019-05-23 23:51:13 +0800

    Create Employee.java

---
+++ Java核心技术/Chapter4/Employee.java
@@ -0,0 +1,45 @@
+package com.horstmann.corejava;
+
+// 该类在这个文件夹并且是这个文件夹的一部分
+// 与PackageTest.java配合使用
+
+import java.time.LocalDate;
+/**
+ * This program demonstrates object construction.
+ * @version V12.0.1 2019-5-23
+ * @author Zhang Xiong
+ */
+public class Employee
+{
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String n,double s,int year,int month,int day)
+    {
+        name = n;
+        salary = s;
+        hireDay = LocalDate.of(year,month,day);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public LocalDate getHireDay()
+    {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent)
+    {
+        double raise = salary*byPercent/100;
+        salary += raise;
+    }
+}
