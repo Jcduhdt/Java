dc413652f5ae2fde0dcc609a9177b86114a83d7a
zhang xiong <15150651313@126.com>
2019-06-22 19:50:49 +0800

    Create DemoB.java

---
+++ java视频代码/包_Jar/packb/DemoB.java
@@ -0,0 +1,13 @@
+package packb;
+
+/**
+ * @version V12.0.1 2019-06-22
+ * @author Zhang Xiong
+ */
+public class DemoB
+{
+    /*public*/protected/*保护权限*/ void method()
+    {
+        System.out.println("demob method run.");
+    }
+}
