436bf4b302e9d3dcb3c5eaef3d72fcb757f1080e
zhang xiong <15150651313@126.com>
2019-06-22 19:50:54 +0800

    Create JarDemo.java

---
+++ java视频代码/包_Jar/pack/JarDemo.java
@@ -0,0 +1,19 @@
+package pack;
+
+/**
+ * @version V12.0.1 2019-06-22
+ * @author Zhang Xiong
+ */
+
+/*
+ * Jar:java的压缩包  跟bandzip winrar差不多
+ * Jar包可以直接应用，不用解压，直接将classpath设置成对应路径 就可以运行了。
+ * cmd 运行jar 就有使用方式，但是在编辑器里还没看怎么用
+ */
+public class JarDemo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Jar!");
+    }
+}
