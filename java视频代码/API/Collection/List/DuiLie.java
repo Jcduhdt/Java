de7a127010796d8d26677987b194f61c8114ef19
zhang xiong <15150651313@126.com>
2019-07-11 22:42:20 +0800

    Create DuiLie.java

---
+++ java视频代码/API/Collection/List/DuiLie.java
@@ -0,0 +1,31 @@
+package cn.itcase.p4.list.demo;
+
+import java.util.LinkedList;
+
+public class DuiLie
+{
+    private LinkedList link;
+    public  DuiLie()//构造函数与类名要相同啊。。
+    {
+        link = new LinkedList();
+    }
+
+    /**
+     * 队列添加元素的功能
+     * @param obj
+     */
+    public void myAdd(Object obj)
+    {
+        link.addLast(obj);
+    }
+
+    public Object myGet()
+    {
+        return link.removeFirst();
+    }
+
+    public boolean isNull()
+    {
+        return link.isEmpty();
+    }
+}
