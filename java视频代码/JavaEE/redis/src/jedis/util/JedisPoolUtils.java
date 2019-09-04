24c741a2948138e500fc915418ccb3d123feae0f
zhang xiong <15150651313@126.com>
2019-09-04 22:29:44 +0800

    redis_jedis
    遇到idea的毛病太多，太难了

---
+++ java视频代码/JavaEE/redis/src/jedis/util/JedisPoolUtils.java
@@ -0,0 +1,51 @@
+package jedis.util;
+
+/**
+ * @author ZhangXiong
+ * @version v12.0.1
+ * @date 2019-09-04
+ */
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * JedisPool工具类
+ *  加载配置文件，配置连接池参数
+ *  提供获取连接的方法
+ */
+public class JedisPoolUtils {
+    private static JedisPool jedisPool;
+
+    static {
+        //读取配置文件
+        InputStream is = JedisPoolUtils.class.getClassLoader().getResourceAsStream("jedis.properties");
+        System.out.println(is);
+        //创建Properties对象
+        Properties pro = new Properties();
+        //关联文件
+        try{
+            pro.load(is);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        //获取数据,设置到JedisPoolConfig中
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(Integer.parseInt(pro.getProperty("maxTotal")));
+        config.setMaxIdle(Integer.parseInt(pro.getProperty("maxIdle")));
+
+        //初始化JedisPool
+        jedisPool = new JedisPool(config,pro.getProperty("host"),Integer.parseInt(pro.getProperty("port")));
+    }
+    /**
+     * 获取连接方法
+     */
+    public static Jedis getJedis(){
+        return jedisPool.getResource();
+    }
+}
